d782f5628f9c94dfd37cefdbbbd18762da04c120
dinaminahi <diankagrechukh@gmail.com>
2019-09-13 11:45:25 +0000

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
-$(".first-heading").fadeOut(220).fadeIn(220);
-$(".second-heading").delay(500).fadeOut(220).fadeIn(220);
-$(".third-heading").delay(1000).fadeOut(220).fadeIn(220);
+
+$('#bear-right').click(function(){
+     $('#bear-right').addClass('animated flip');
+     $('#bear-right').removeClass('animated flip');
+});
